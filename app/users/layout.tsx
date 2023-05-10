61989b21efc887e2f5afc124a06a1e040177d53b
Ankush <000.kumarankush@gmail.com>
2023-05-10 22:14:07 +0530

    add: people section

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,19 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 interface UsersLayoutProps {
   children: React.ReactNode;
 }
 const UsersLayout = async ({ children }: UsersLayoutProps) => {
+  const users = await getUsers();
   return (
     // @ts-expect-error Server Component
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList users={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 };
